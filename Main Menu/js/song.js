f74b277749664c3ad479f862c07703b03ae542bd
Jasmin Voigtlander <Jasmin@Jasmins-MacBook-Air.local>
2018-11-28 11:16:14 -0500

    added the a song

--- Main Menu/js/song.js
+++ Main Menu/js/song.js
@@ -1,5 +1,7 @@
 DoMi.song = function(game) {};
 
+var homeBtn, freeplayBtn, playlistBtn, musicOn, musicOff, continueBtn, playBtn, pauseBtn, music;
+
 DoMi.song.prototype = {
 
     preload: function () {
@@ -8,13 +10,21 @@ DoMi.song.prototype = {
         game.load.image('playlistBtn', 'icons/playlist.png');
         game.load.image('musicOn', 'icons/toggle-on.png');
         game.load.image('musicOff', 'icons/toggle-off.png');
+        game.load.image('playBtn', 'icons/play.png');
+        game.load.image('pauseBtn', 'icons/pause.png');
         game.load.image('continueBtn', 'icons/continue.png');
         game.load.image('freeplayBG', 'images/free-play/freeplay-BG.png');
+        game.load.image('asongTitle', 'images/song/a-song.png');
+        game.load.audio('a-song', 'audio/song/a-song.mp3');
     },
 
     create: function () {
         //Add background and slider base
         game.add.image(0, -1, 'freeplayBG');
+        game.add.image(275, 220, 'asongTitle');
+        
+        //Add The A Song
+        music = game.add.audio('a-song');
 
         //Instantiate buttons
         homeBtn = game.add.button(48, 30, 'homeBtn', actionOnClick1, this);
@@ -38,13 +48,25 @@ DoMi.song.prototype = {
             musicOn.onInputOver.add(over, this);
             musicOn.onInputOut.add(out, this);
         } else {
-           musicOff = game.add.button(1250, 40, 'musicOff', actionOnClick4, this);
+            musicOff = game.add.button(1250, 40, 'musicOff', actionOnClick4, this);
             musicOff.alpha = 0.85;
             musicOff.onInputOver.add(over, this);
             musicOff.onInputOut.add(out, this); 
         }
 
-        continueBtn = game.add.button(1179, 587, 'continueBtn', actionOnClick5, this);
+        if (music.isPlaying === false) {
+            playBtn = game.add.button(540, 330, 'playBtn', actionOnClick5, this);
+            playBtn.alpha = 0.85;
+            playBtn.onInputOver.add(over, this);
+            playBtn.onInputOut.add(out, this);
+        } else {
+            pauseBtn = game.add.button(540, 330, 'playBtn', actionOnClick5, this);
+            pauseBtn.alpha = 0.85;
+            pauseBtn.onInputOver.add(over, this);
+            pauseBtn.onInputOut.add(out, this);
+        }
+
+        continueBtn = game.add.button(1179, 587, 'continueBtn', actionOnClick6, this);
         continueBtn.alpha = 0.85;
         continueBtn.onInputOver.add(over, this);
         continueBtn.onInputOut.add(out, this);
@@ -101,6 +123,33 @@ DoMi.song.prototype = {
         }
 
         function actionOnClick5(event) {
+            if (event.key == 'playBtn') {
+                playBtn.destroy();
+                pauseBtn = game.add.button(540, 330, 'playBtn', actionOnClick5, this);
+                pauseBtn.alpha = 0.85;
+                pauseBtn.onInputOver.add(over, this);
+                pauseBtn.onInputOut.add(out, this);
+                BGM.pause();
+                if (music.isPlaying === true) {
+                    music.resume();
+                } else {
+                    music.play()
+                }
+            } else if (event.key == 'pauseBtn') {
+                pauseBtn.destroy();
+                playBtn = game.add.button(540, 330, 'playBtn', actionOnClick5, this);
+                playBtn.alpha = 0.85;
+                playBtn.onInputOver.add(over, this);
+                playBtn.onInputOut.add(out, this);
+                BGM.resume();
+                music.pause();
+            }
+            music.onStop.addOnce(function() {
+                
+            });
+        }
+
+        function actionOnClick6(event) {
             game.state.start("bubblePop");
         }
     }
